b22b6b91ce8fdbf03e141d5c0aa62d3c909a6f0c
Sainava <sainava.modak@gmail.com>
2025-12-01 22:39:25 +0530

    Add floating astronaut and stars animation to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,59 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-black font-sans">
-      <main className="flex w-full max-w-3xl flex-col items-center gap-12 py-32 px-16 bg-black">
+    <div className="flex min-h-screen items-center justify-center bg-black font-sans relative overflow-hidden">
+      {/* Animated Stars Background */}
+      <div className="box-of-star1">
+        <div className="star star-position1"></div>
+        <div className="star star-position2"></div>
+        <div className="star star-position3"></div>
+        <div className="star star-position4"></div>
+        <div className="star star-position5"></div>
+        <div className="star star-position6"></div>
+        <div className="star star-position7"></div>
+      </div>
+      <div className="box-of-star2">
+        <div className="star star-position1"></div>
+        <div className="star star-position2"></div>
+        <div className="star star-position3"></div>
+        <div className="star star-position4"></div>
+        <div className="star star-position5"></div>
+        <div className="star star-position6"></div>
+        <div className="star star-position7"></div>
+      </div>
+      <div className="box-of-star3">
+        <div className="star star-position1"></div>
+        <div className="star star-position2"></div>
+        <div className="star star-position3"></div>
+        <div className="star star-position4"></div>
+        <div className="star star-position5"></div>
+        <div className="star star-position6"></div>
+        <div className="star star-position7"></div>
+      </div>
+      <div className="box-of-star4">
+        <div className="star star-position1"></div>
+        <div className="star star-position2"></div>
+        <div className="star star-position3"></div>
+        <div className="star star-position4"></div>
+        <div className="star star-position5"></div>
+        <div className="star star-position6"></div>
+        <div className="star star-position7"></div>
+      </div>
+      
+      {/* Floating Astronaut */}
+      <div data-js="astro" className="astronaut">
+        <div className="head"></div>
+        <div className="arm arm-left"></div>
+        <div className="arm arm-right"></div>
+        <div className="body">
+          <div className="panel"></div>
+        </div>
+        <div className="leg leg-left"></div>
+        <div className="leg leg-right"></div>
+        <div className="schoolbag"></div>
+      </div>
+
+      <main className="flex w-full max-w-3xl flex-col items-center gap-12 py-32 px-16 relative z-20">
         <div className="voltage-heading">
           <h1>Open Source Contributions</h1>
           <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 234.6 61.3" preserveAspectRatio="none" xmlSpace="preserve">
